05a17a93e5738919cb191972db99f4b590ef7e53
craster69 <tirefilms99@gmail.com>
2024-07-23 12:28:48 +0300

    DebugTg

--- web/app/mini-shop/src/components/App.jsx
+++ web/app/mini-shop/src/components/App.jsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+
+const tg = window.Telegram.WebApp
+
+console.log(tg)
 
 export const App = () => {
+
+  useEffect(() => {
+
+    tg.ready()
+
+  }, [])
+
   return (
     <>
         привет
